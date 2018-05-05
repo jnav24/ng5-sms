e9f9f277edd4c50fd7d7f6a8809e075c4e77c74f
Justin Navarro <justinn@liveanswer.com>
2018-05-05 09:27:55 -0400

    Initial firebase setup completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {RouterModule} from '@angular/router';
+import {AngularFireModule} from 'angularfire2';
 
 // Config
 import { RouterConfig } from '@app/config/router.config';
+import {firebaseConfig} from '@app/config/firebase.config';
 
 // Components
 import { AppComponent } from '@app/app.component';
@@ -13,12 +15,13 @@ import { MessagesComponent } from '@app/messages/messages.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MessagesComponent
+      AppComponent,
+      MessagesComponent
   ],
   imports: [
-    BrowserModule,
-    RouterModule.forRoot(RouterConfig),
+      AngularFireModule.initializeApp(firebaseConfig),
+      BrowserModule,
+      RouterModule.forRoot(RouterConfig),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/config/firebase_sample.config.ts
@@ -0,0 +1,8 @@
+export const firebaseSampleConfig = {
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: ''
+};
