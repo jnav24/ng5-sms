3ae8c34f429f0ab9074e0b458a70de611cc9e391
Justin Navarro <justinn@liveanswer.com>
2018-05-10 00:29:47 -0400

    allow to receive sms to the app

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -19,14 +19,104 @@ async function twilioCreds() {
     }
 }
 
+async function getUser(userNumber) {
+    try {
+        return await admin.firestore()
+            .collection(`users`)
+            .where('preferred', '==', userNumber)
+            .get();
+    } catch (err) {
+        const errMsg = "Unable to retrieve user";
+        console.error(errMsg);
+        throw errMsg;
+    }
+}
+
+async function getContact(contactNumber) {
+    try {
+        return await admin.firestore()
+            .collection(`contacts`)
+            .where('preferred', '==', contactNumber)
+            .get();
+    } catch (err) {
+        const errMsg = "Unable to retrieve contact";
+        console.error(errMsg);
+        throw errMsg;
+    }
+}
+
 export const retrieveSms = functions.https.onRequest((request, response) => {
-    const twiml = Twilio.twiml.MessagingResponse;
-    const mresponse = new twiml();
-    mresponse.message('The Robots are coming! Head for the hills!');
-    console.log(request.method);
-    console.log(request.query);
-    console.log(twiml);
-    console.log(mresponse);
+    const twiml = new Twilio.twiml.MessagingResponse();
+    let msg = 'Unable to send message. Must be a post.';
+    let statusCode = 403;
+
+    if (request.method === 'POST' && Object.keys(request.body).length) {
+        console.log(request.body);
+        const smsTo = request.body.To;
+        const smsFrom = request.body.From;
+        const smsMsg = request.body.Body;
+        let errMsg = '';
+        msg = 'Message received successfully';
+        statusCode = 200;
+
+        getUser(smsTo).then(userSnapshot => {
+            const user_message_ids = [];
+            const contact_message_ids = [];
+
+            userSnapshot.forEach(user => {
+                user.data()['message_ids'].map(id => {
+                    user_message_ids.push(id);
+                });
+            });
+
+            getContact(smsFrom).then(contactSnapshot => {
+                if (contactSnapshot.size) {
+                    contactSnapshot.forEach(contact => {
+                        contact.data()['message_ids'].map(id => {
+                            contact_message_ids.push(id);
+                        });
+                    });
+
+                    const message_ids = user_message_ids.filter(id => {
+                        return contact_message_ids.indexOf(id) > -1;
+                    });
+
+                    if (message_ids.length) {
+                        const mid = message_ids.shift();
+
+                        admin.firestore()
+                            .doc(`messages/${mid}`)
+                            .collection('conversation')
+                            .add({
+                                to: smsTo,
+                                from: smsFrom,
+                                message: smsMsg,
+                                state: 'unread',
+                                created: Math.round((new Date()).getTime() / 1000).toString()
+                            })
+                            .catch(err => {
+                                errMsg = 'Unable to add conversation to message' + err;
+                                console.error(errMsg);
+                            });
+                    }
+                } else {
+                    // create new message/conversation
+                    // create new contact and add message id
+                    // add message id to user
+                }
+            })
+            .catch(err => {
+                console.error(err);
+            });
+        })
+        .catch(err => {
+            console.error(err);
+        });
+    }
+
+    twiml.message(msg);
+    response.writeHead(statusCode, {'Content-Type': 'text/xml'});
+    response.end();
 });
 
 exports.sendSms = functions.firestore.document('messages/{message_id}/conversation/{conversation_id}')
