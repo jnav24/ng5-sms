f07af0d99e7de0df0a51e749a5e8cca252f0be76
Justin Navarro <info@justinnavarro.net>
2018-08-16 16:14:38 -0400

    attempt to have a full page messenger and add user state to the chat

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Store} from 'ngxs';
+import {UserInterface} from '@app/common/interfaces/user.interface';
+import {Subscription} from 'rxjs/Subscription';
 
 @Component({
-  selector: 'app-chat',
-  templateUrl: './chat.component.html',
-  styleUrls: ['./chat.component.scss']
+    selector: 'app-chat',
+    templateUrl: './chat.component.html',
+    styleUrls: ['./chat.component.scss']
 })
-export class ChatComponent implements OnInit {
+export class ChatComponent implements OnInit, OnDestroy {
+    private userSubscription: Subscription;
+    public user: UserInterface;
 
-  constructor() { }
+    constructor(private store: Store) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.userSubscription = this.store
+            .select(state => state.user)
+            .subscribe(user => {
+                this.user = user;
+                console.log(user);
+            });
+    }
 
+    ngOnDestroy() {
+        this.userSubscription.unsubscribe();
+    }
 }
